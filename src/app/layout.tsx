06e494b48f299f299f26020e883b0cd8f43a9194
Getaye Temesgen <tgech71@gmail.com>
2025-09-18 06:06:03 +0000

    SyntaxError: Missing initializer in destructuring declaration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,14 @@
+
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import React from 'react';
 import { Providers } from './providers';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'EZBus',
+  description: 'The easiest way to book your bus tickets.',
+};
 
 export default function RootLayout({
   children,
@@ -10,9 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <title>EZBus</title>
-        <meta name="description" content="The easiest way to book your bus tickets." />
+       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
