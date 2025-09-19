4700967c2bccf6f482a622f26e0b2968e41c4529
Getaye Temesgen <tgech71@gmail.com>
2025-09-19 15:56:41 +0000

    Error: Route "/" used `cookies().get('auth_session')`. `cookies()` shoul

--- src/app/lib/auth.ts
+++ src/app/lib/auth.ts
@@ -54,16 +54,14 @@ export const validateRequest = cache(
 
 		const result = await lucia.validateSession(sessionId);
 		// next.js throws when you attempt to set cookie when rendering page
-		try {
-			if (result.session && result.session.fresh) {
-				const sessionCookie = lucia.createSessionCookie(result.session.id);
-				cookieStore.set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
-			}
-			if (!result.session) {
-				const sessionCookie = lucia.createBlankSessionCookie();
-				cookieStore.set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
-			}
-		} catch {}
+		if (result.session && result.session.fresh) {
+			const sessionCookie = lucia.createSessionCookie(result.session.id);
+			cookieStore.set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+		}
+		if (!result.session) {
+			const sessionCookie = lucia.createBlankSessionCookie();
+			cookieStore.set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+		}
 		return result;
 	}
 );
