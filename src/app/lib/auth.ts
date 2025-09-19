3bea75970e235660bec2c36311a5756391a773cb
Getaye Temesgen <tgech71@gmail.com>
2025-09-19 11:48:46 +0000

    Build Error
    Module not found: Can't resolve './prisma'
    
    ./src/app/lib/a

--- src/app/lib/auth.ts
+++ src/app/lib/auth.ts
@@ -1,5 +1,5 @@
 import { PrismaAdapter } from "@lucia-auth/adapter-prisma";
-import prisma from "./prisma";
+import prisma from "@/lib/prisma";
 import { Lucia, Session, User } from "lucia";
 import { cookies } from "next/headers";
 import { cache } from "react";
