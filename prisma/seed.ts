246219aa55a09519be3b4129a6fd807bfbf78881
gech71 <tgech71@gmail.com>
2025-09-22 12:13:46 +0300

    remove unused Prisma schema files and update seed script to ensure seat layouts are deleted

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -35,8 +35,8 @@ async function main() {
   await prisma.route.deleteMany();
   await prisma.discount.deleteMany();
   await prisma.seat.deleteMany();
-  await prisma.seatLayout.deleteMany();
   await prisma.bus.deleteMany();
+  await prisma.seatLayout.deleteMany();
   await prisma.commissionTier.deleteMany();
   await prisma.location.deleteMany();
   await prisma.user.deleteMany();
