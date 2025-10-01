c84fdea35704bd2f21fe67812a6fe72b3ddcf210
Getaye Temesgen <tgech71@gmail.com>
2025-10-01 07:40:43 +0000

    HTTP Strict Transport Security (HSTS) is a web security policy mechanism

--- next.config.ts
+++ next.config.ts
@@ -46,6 +46,10 @@ const nextConfig: NextConfig = {
             key: 'Content-Security-Policy',
             value: contentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
           },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
         ],
       },
     ];
