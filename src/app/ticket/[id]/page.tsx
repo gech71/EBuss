49cd4bb8d81c605ffbffadc61b434e89e18ac458
Getaye Temesgen <tgech71@gmail.com>
2025-12-16 07:30:57 +0000

    ⨯ TypeError: (0 , i.cookies)(...).get is not a function     at j (.next\

--- src/app/ticket/[id]/page.tsx
+++ src/app/ticket/[id]/page.tsx
@@ -12,7 +12,7 @@ interface TicketPageProps {
 }
 
 async function getIsMiniApp() {
-  const cookieStore = cookies();
+  const cookieStore = await cookies();
   const sessionCookie = cookieStore.get("miniapp_session");
   if (sessionCookie) {
     try {
